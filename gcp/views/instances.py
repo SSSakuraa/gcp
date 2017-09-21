55d01a4e94b0c6ddfef1164a9e32061c78422bab
1172975869@qq.com <1172975869@qq.com>
2017-09-21 13:36:39 +0800

    region

--- gcp/views/instances.py
+++ gcp/views/instances.py
@@ -345,16 +345,15 @@ def batch_info():
 def instances_fee():
     try:
         from gcp import fee
-        from regions import *
+        import regions
         auth=Auth()
         service=auth.get_service(request)
         ebs=request.args.get('ebs')
         instance_type=request.args.get('instance_type')
-        region=auth.region
+        region_id=auth.region
         os=request.args.get('os')
         quantity=request.args.get('quantity')
-        
-        total_compute=(fee.price_dict[instance_type]['price'][get_region_id(region)])*quantity
+        total_compute=(fee.price_dict[instance_type]['price'][get_region_name(region_id)])*quantity
         total_ebs=0
         total=total_compute+total_ebs
         res={
