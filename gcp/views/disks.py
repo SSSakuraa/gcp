e50bf9eee4ecd9cd19bd6aa72dfd7ab8b2d87b5d
1172975869@qq.com <1172975869@qq.com>
2017-09-27 17:17:35 +0800

    disk

--- gcp/views/disks.py
+++ gcp/views/disks.py
@@ -12,53 +12,73 @@ disks = Blueprint('disks', __name__)
 
 # function interface for google API
 def gcp_disk_func(func_name, param):
-    service = param['service']
-    project = param['project']
-    zone = param['zone']
-
     if func_name == "disk_insert":
-        import rstr
-        disk_list=gcp_disk_func("disk_list",param)
-        while True:
-            name = rstr.xeger('[a-z](?:[-a-z0-9]{0,61}[a-z0-9])?')
-            if name not in disk_list:
-                break
-        disk_body = {
-            "name": name,
-            "sizeGb": param['sizeGb'],
-            "storageType": param['storageType']
-        }
+        return gcp_disk_insert(param)
 
-        myrequest = service.disks().insert(project=project, zone=zone, body=disk_body)
+    if func_name == "disk_insert_batch":
+        return gcp_disk_insert_batch(param)
+
+    if func_name == "disk_list":
+        return gcp_disk_list(param)
+
+    if func_name == "disk_info":
+        return gcp_disk_info(param)
+
+
+def gcp_disk_insert(param):
+    import rstr
+    disk_list = gcp_disk_func("disk_list", param)
+    while True:
+        name = rstr.xeger('[a-z](?:[-a-z0-9]{0,61}[a-z0-9])?')
+        if name not in disk_list:
+            break
+    disk_body = {
+        "name": name,
+        "sizeGb": param['sizeGb'],
+        "storageType": param['storageType']
+    }
+    myrequest = param['service'].disks().insert(
+        project=param['project'], zone=param['zone'], body=disk_body)
+    myresponse = myrequest.execute()
+    res = {
+        'id': myresponse['targetId'],
+        'status': myresponse['status'],
+        'name': myresponse['targetLink'].split('/disks/')[1]
+    }
+    return res
+
+
+def gcp_disk_insert_batch(param):
+    return
+
+
+
+
+# get disk name for all disks
+def gcp_disk_list(param):
+    myrequest = param['service'].disks().list(
+        project=param['project'], zone=param['zone'])
+    disk_list = []
+    while myrequest is not None:
         myresponse = myrequest.execute()
-        res={
-            'id':myresponse['targetId'],
-            'status':myresponse['status'],
-            'name':myresponse['targetLink'].split('/disks/')[1],
-        }
-        return res
-    
-    if func_name=="disk_list":
-        myrequest = service.disks().list(project=project, zone=zone)
-        disk_list=[]
-        while myrequest is not None:
-            myresponse = myrequest.execute()
-            for disk in myresponse['items']:
-                # TODO: Change code below to process each `disk` resource:
-                disk_list.append(disk['name'])
+        for disk in myresponse['items']:
+            # TODO: Change code below to process each `disk` resource:
+            disk_list.append(disk['name'])
 
-            myrequest = service.disks().list_next(previous_request=myrequest, previous_response=myresponse)
-        return disk_list
+        myrequest = param['service'].disks().list_next(
+            previous_request=myrequest, previous_response=myresponse)
+    return disk_list
 
-    if func_name=="disk_info":
-        disk=param['disk_name']
-        myrequest = service.disks().get(project=project, zone=zone, disk=disk)
-        myresponse = myrequest.execute()
-        res={
-            'id':myresponse['id'],
-            'create_time':myresponse['creationTimestamp'],
-            'size':myresponse['sizeGb'],
-            'status':myresponse['status'],
-            'type':myresponse['type'].split('/diskTypes/')[1]
-        }
-        return res
+
+def gcp_disk_info(param):
+    myrequest = param['service'].disks().get(
+        project=param['project'], zone=param['zone'], disk=param['disk_name'])
+    myresponse = myrequest.execute()
+    res = {
+        'id': myresponse['id'],
+        'create_time': myresponse['creationTimestamp'],
+        'size': myresponse['sizeGb'],
+        'status': myresponse['status'],
+        'type': myresponse['type'].split('/diskTypes/')[1]
+    }
+    return res
